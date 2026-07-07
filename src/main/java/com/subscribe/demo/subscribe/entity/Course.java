b9f81608a80f356471dbfb8dbcc3a0e7265eff94
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-07-07 20:10:54 +0300

    Feat(entity) : create entity

---
+++ src/main/java/com/subscribe/demo/subscribe/entity/Course.java
@@ -0,0 +1,27 @@
+package com.subscribe.demo.subscribe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "course")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Course {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @EqualsAndHashCode.Include
+  private UUID id;
+
+  private String title;
+  private Instant startDate;
+  private Instant endDate;
+}
---
+++ src/main/java/com/subscribe/demo/subscribe/entity/SubscriptionEmailRequested.java
@@ -0,0 +1,29 @@
+package com.subscribe.demo.subscribe.entity;
+
+import com.course.demo.endpoint.event.model.PojaEvent;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Duration;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionEmailRequested extends PojaEvent {
+  private String to;
+  private String userFirstName;
+  private String courseTitle;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(30);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(5);
+  }
+}
