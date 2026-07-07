dafa95dd04e11653611d08b3b2bcb46c7fca1b18
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-07-07 20:42:51 +0300

    Fix error datasource

--- src/main/java/com/subscribe/demo/db/DatabaseConfig.java
+++ src/main/java/com/subscribe/demo/db/DatabaseConfig.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataSource {
+@Configuration
+public class DatabaseConfig {
   @Value("${spring.datasource.url}")
   private String neonUrl;
 
