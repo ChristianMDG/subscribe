0c639d966ed71c8da972af3dc7c96782743d5933
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-07-09 00:52:49 +0300

    feat(Subscribe) : userRequest

--- src/main/java/com/subscribe/demo/subscribe/dto/UserRequest.java
+++ src/main/java/com/subscribe/demo/subscribe/dto/UserRequest.java
@@ -1,13 +1,18 @@
 package com.subscribe.demo.subscribe.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRequest {
-  private String firstName;
-  private String lastName;
-  private String userName;
-  private String email;
+  @NotBlank private String firstName;
+
+  @NotBlank private String lastName;
+
+  @NotBlank private String userName;
+
+  @NotBlank @Email private String email;
 }
