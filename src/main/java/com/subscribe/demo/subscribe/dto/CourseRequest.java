f07ff718ebc5deb2a234b8672760ebd5604374cf
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-07-08 23:58:23 +0300

    feat(Subscribe) : remove condition

--- src/main/java/com/subscribe/demo/subscribe/dto/CourseRequest.java
+++ src/main/java/com/subscribe/demo/subscribe/dto/CourseRequest.java
@@ -1,8 +1,5 @@
 package com.subscribe.demo.subscribe.dto;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CourseRequest {
-  @NotBlank private String title;
-  @NotNull @Future private Instant startDate;
-  @NotNull private Instant endDate;
+  private String title;
+  private Instant startDate;
+  private Instant endDate;
 }
