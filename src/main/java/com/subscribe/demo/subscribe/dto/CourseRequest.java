d688f4cddc3aede1cc6c57a82ca2017262030ed2
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-07-08 23:39:04 +0300

    feat(Subscribe) : remove

--- src/main/java/com/subscribe/demo/subscribe/dto/CourseRequest.java
+++ src/main/java/com/subscribe/demo/subscribe/dto/CourseRequest.java
@@ -1,7 +1,6 @@
 package com.subscribe.demo.subscribe.dto;
 
 import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
@@ -14,8 +13,4 @@ public class CourseRequest {
   @NotBlank private String title;
   @NotNull @Future private Instant startDate;
   @NotNull private Instant endDate;
-
-  @NotNull
-  @Min(1)
-  private Integer maxParticipants;
 }
